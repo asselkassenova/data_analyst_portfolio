773dda01d5f331028c711dc00b2726f15aa63d85
asselkassenova <130527153+asselkassenova@users.noreply.github.com>
2023-12-22 16:58:29 -0500

    Update app_recomendation.py

--- Insurance_price/app_recomendation.py
+++ Insurance_price/app_recomendation.py
@@ -3,9 +3,7 @@
 # %%
 import pandas as pd              
 import numpy as np                        
-import matplotlib.pyplot as plt  
-import matplotlib.dates as mdates
-from tqdm.notebook import tqdm   
+  
 
 df = pd.read_csv('Medical_Insurance_dataset.csv')
 data = df
